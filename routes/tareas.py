cc5635fa1ae5c88b57804717aee966b0987fba51
Lizet Lopez <lizet.chlopez@gmail.com>
2025-08-28 20:50:18 -0600

    k

--- routes/tareas.py
+++ routes/tareas.py
@@ -9,3 +9,28 @@ tareas_bp = Blueprint('tareas', __name__)
 @tareas_bp.route('/obtener', methods=['GET'])
 def get():
     return jsonify({"mensaje": "Estas son tus tareas"})
+
+# endpoint para crear tarea (POST) datos desde el bpdy
+
+
+@tareas_bp.route('/crear', methods=['POST'])
+def crear():
+    data = request.get_json()
+    nombre = data.get('nombre')
+    apellido = data.get('apellido')
+
+    return jsonify({"saludo": f"Hola {nombre} {apellido} "})
+
+
+# crear endpoint para actualizar (PUT)
+@tareas_bp.route('/modificar/<int:user_id>', methods=['PUT'])
+def modificar(user_id):
+
+    # obtenemos datos del body
+    data = request.get_json()
+    nombre = data.get('nombre')
+    apellido = data.get('apellido')
+
+    mensaje = f"Usuario con id: {user_id} modificado a {nombre} {apellido}"
+
+    return jsonify({"saludo": mensaje})
